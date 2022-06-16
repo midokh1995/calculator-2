e79cea5fc71c5309ae91f5da7ba568bde9093a4e
midokh1995 <106180410+midokh1995@users.noreply.github.com>
2022-06-16 20:06:17 +0300

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:salaty/view/home.dart';
+// اسم المشوع بحاول اغيره ولكن بديني خطأ وحليا المشروع شغال على وضعه الحالي
 
 void main() {
   runApp(MaterialApp(
